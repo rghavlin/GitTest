2fe560fffaf6fd42200eefa78d4b7848a558ba28
rghavlin <rghavlin@gmail.com>
2017-12-16 06:13:21 -0600

    Test 1

---
+++ app/src/main/java/com/example/gittest/gittest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gittest.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    String gitCommit1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
